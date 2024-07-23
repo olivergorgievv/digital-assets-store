f3ae3cbc5ea4e93073ed3315cc944aba140c6fa2
Oliver <oliver.gorgiev11@gmail.com>
2024-07-23 20:13:50 +0200

    env

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -8,6 +8,16 @@ dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
+});
+
 let cached = (global as any).payload;
 
 if (!cached) {
@@ -32,6 +42,11 @@ export const getPayloadClient = async ({
   }
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "onboarding@resend.com",
+        fromName: "DigitalAstro",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
